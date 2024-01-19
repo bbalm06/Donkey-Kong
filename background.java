0a4ebd280a86ccc9a8dac4392c29a90dd0e2daa4
Brandon Balmos <98839318+bbalm06@users.noreply.github.com>
2024-01-19 13:51:18 -0600

    Update background.java

--- background.java
+++ background.java
@@ -2,7 +2,6 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.swing.*;
@@ -32,7 +31,7 @@ public class background extends JPanel implements Runnable , MouseListener, Mous
     public static int[] y7 = {265,245,235,255};
     public static Polygon n7 = new Polygon(x7,y7,4);
     public static int[] x8 = {0,0,335,335};
-    public static int[] y8 = {255,235,235,255};
+    public static int[] y8 = {255,233,233,255};
     public static Polygon n8 = new Polygon(x8,y8,4);
     public static int[] x9 = {315,315,205,205};
     public static int[] y9 = {175,155,155,175};
@@ -41,39 +40,92 @@ public class background extends JPanel implements Runnable , MouseListener, Mous
     static Rectangle hitB = new Rectangle(Mario.x,Mario.y,32,32);
     static Rectangle hitBo = new Rectangle(Barrel.x,Barrel.y,32,32);
 
-    public static Image barrelRoll = Toolkit.getDefaultToolkit().getImage("barrelRoll.png");
-    public static Image marioLookRight = Toolkit.getDefaultToolkit().getImage("marioLookRight.png");
-    public static Image marioLookLeft = Toolkit.getDefaultToolkit().getImage("marioLookLeft.png");
-    public static Image marioRunRight1 = Toolkit.getDefaultToolkit().getImage("marioRunRight1.png");
-    public static Image marioRunRight2 = Toolkit.getDefaultToolkit().getImage("marioRunRight2.png");
-    public static Image marioRunLeft1 = Toolkit.getDefaultToolkit().getImage("marioRunLeft1.png");
-    public static Image marioRunLeft2 = Toolkit.getDefaultToolkit().getImage("marioRunLeft2.png");
+    public static Image marioLookRight = Toolkit.getDefaultToolkit().getImage("C:\\Users\\bbalm\\IdeaProjects\\Donkey Kong\\marioLookRight.png");
+    public static Image marioLookLeft = Toolkit.getDefaultToolkit().getImage("C:\\Users\\bbalm\\IdeaProjects\\Donkey Kong\\marioLookLeft.png");
+    public static Image marioRunRight1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\bbalm\\IdeaProjects\\Donkey Kong\\marioRunRight1.png");
+    public static Image marioRunRight2 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\bbalm\\IdeaProjects\\Donkey Kong\\marioRunRight2.png");
+    public static Image marioRunLeft1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\bbalm\\IdeaProjects\\Donkey Kong\\marioRunLeft1.png");
+    public static Image marioRunLeft2 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\bbalm\\IdeaProjects\\Donkey Kong\\marioRunLeft2.png");
     public static Image currentAnimation;
-    public boolean first = true;
+
+    public static int[] lX1 = {431-5,450+5,450+5,431-5};
+    public static int[] lY1 = {645-45,645-45,683-20,683-20};
+    public static Polygon LAD1 = new Polygon(lX1,lY1,4);
+    public static int[] lX2 = {225-5,245+5,245-5,225+5};
+    public static int[] lY2 = {545-46,545-46,605-20,605-20};
+    public static Polygon LAD2 = new Polygon(lX2,lY2,4);
+
+    public static int[] lX3 = {75-5,94+5,94+5,75-5};
+    public static int[] lY3 = {555-46,555-46,595-20,595-20};
+    public static Polygon LAD3 = new Polygon(lX3,lY3,4);
+
+    public static int[] lX4 = {75-5,94+5,94+5,75-5};
+    public static int[] lY4 = {555-45,555-45,595-20,595-20};
+    public static Polygon LAD4 = new Polygon(lX4,lY4,4);
+
+    public static int[] lX5 = {263-5,280+5,280+5,263-5};
+    public static int[] lY5 = {453-45,453-45,514-20,514-20};
+    public static Polygon LAD5 = new Polygon(lX5,lY5,4);
+    public static int[] lX6 = {430+5,450-5,450-5,430+5};
+    public static int[] lY6 = {462-45,462-45,500-20,500-20};
+    public static Polygon LAD6 = new Polygon(lX6,lY6,4);
+    public static int[] lX7 = {168+2-5,184+2+5,184+2+5,168+2-5};
+    public static int[] lY7 = {369-45,369-45,420-20,420-20};
+    public static Polygon LAD7 = new Polygon(lX7,lY7,4);
+    public static int[] lX8 = {76-5,92+5,92+5,76-5};
+    public static int[] lY8 = {374-45,374-45,410-20,410-20};
+    public static Polygon LAD8 = new Polygon(lX8,lY8,4);
+    public static int[] lX9 = {433-5,448+5,448+5,433-5};
+    public static int[] lY9 = {285-45,285-45,322-20,322-20};
+    public static Polygon LAD9 = new Polygon(lX9,lY9,4);
+
+    public static ArrayList<Polygon> ladderHitBoxes;
     public background(){
         new Thread(this).start();
     }
     public void paintComponent(Graphics window){
         Graphics2D g2 = (Graphics2D) window;
         g2.fillRect(Mario.x,Mario.y,33,33);
-        g2.fillPolygon(n1);
-        g2.fillPolygon(n2);
-        g2.fillPolygon(n3);
-        g2.fillPolygon(n4);
-        g2.fillPolygon(n5);
-        g2.fillPolygon(n6);
-        g2.fillPolygon(n7);
-        g2.fillPolygon(n8);
-        g2.fillPolygon(n9);
+//        g2.fillPolygon(n1);
+//        g2.fillPolygon(n2);
+//        g2.fillPolygon(n3);
+//        g2.fillPolygon(n4);
+//        g2.fillPolygon(n5);
+//        g2.fillPolygon(n6);
+//        g2.fillPolygon(n7);
+//        g2.fillPolygon(n8);
+//        g2.fillPolygon(n9);
+//        g2.fillPolygon(LAD1);
+//        g2.fillPolygon(LAD2);
+//        g2.fillPolygon(LAD3);
+//        g2.fillPolygon(LAD4);
+//        g2.fillPolygon(LAD5);
+//        g2.fillPolygon(LAD6);
+//        g2.fillPolygon(LAD7);
+//        g2.fillPolygon(LAD8);
+        //g2.fillPolygon(LAD9);
+
+
         hitB = new Rectangle(Mario.x,Mario.y,37,37);
-        Image img1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\s866250\\IdeaProjects\\Donkey Kong\\donkeyStage.png"); /*the image cannot be in the SRC folder*/
+        hitBo = new Rectangle(Barrel.x,Barrel.y,37,37);
+        Image img1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\bbalm\\IdeaProjects\\Donkey Kong\\donkeyStage (1).png"); /*the image cannot be in the SRC folder*/
         g2.drawImage(img1, 0 , 0 , 525 , 700 , this);
         g2.drawImage(currentAnimation, Mario.x, Mario.y,32,32,this);
-        g2.drawImage(barrelRoll, Barrel.x , Barrel.y , 32 , 32 , this);
         window.setColor(Color.WHITE);
         //window.drawString("Mouse  coordinates " + "(" + MouseInfo.getPointerInfo().getLocation().x + "   " + MouseInfo.getPointerInfo().getLocation().y + ")", 250, 30 );
         window.setColor(Color.red);
-        first = false;
+        g2.fillPolygon(LAD9);
+        //first = false;
+    }
+
+    public static int smallest(int[] x){
+        int smol  = 88888;
+        for(int y : x){
+            if(y<smol){
+                smol = y;
+            }
+        }
+        return smol;
     }
     public void run()
     {
@@ -84,19 +136,33 @@ public class background extends JPanel implements Runnable , MouseListener, Mous
             {
 
                 if(Mario.isIntersecting(hitBoxes) ){
-                    Mario.y -= 1;
-                    Mario.ran = true;
+                    if(Mario.y > smallest(Mario.colliding.ypoints)){
+                        System.out.println(smallest(Mario.colliding.ypoints) + " " + Mario.y);
+                        Mario.y +=2;
+                    }
+                    Barrel.move();
+                    else{
+                        Mario.y -= 1;
+                    }
+
+                    if(Mario.isClimbing(background.ladderHitBoxes)){
+                        Mario.y-=3;
+                    }
                 }
                 if(! Mario.isIntersecting(hitBoxes) ){
                     Mario.y+=1;
-
+                    Barrel.move();
                 }
-                if(!Barrel.isIntersecting(hitBoxes)){
+                if(! Barrel.isIntersecting(hitBoxes)){
                     Barrel.y+=1;
+                    Barrel.move();
                 }
 
 
-                sleep(20);
+
+
+
+                sleep(15);
                 repaint();
             }
         }
