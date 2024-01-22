ca597d96b970a6367f9086b5c86413b3a63d008c
Felix Everlong <152215467+mewwickbwyant@users.noreply.github.com>
2024-01-22 13:08:37 -0600

    Add files via upload

--- Barrel.java
+++ Barrel.java
@@ -11,45 +11,41 @@ import javax.swing.Timer;
 import static java.lang.Thread.currentThread;
 import static java.lang.Thread.sleep;
 
-public class Barrel extends JPanel implements Runnable
-{
+public class Barrel extends JPanel implements Runnable {
     public static Polygon colliding;
     public static int dx;
     public static int dy;
-    public static int x= 163;
-    public static int y= 198;
+    public static int x = 163;
+    public static int y = 196;
     public static boolean Facing = true; //True = Right, False = Left
-    public static boolean move = false;
-    public static boolean isTouching =true;
 
     public static boolean isIntersecting(ArrayList<Polygon> hit) {
         for (Polygon x : hit) {
             if (x.intersects(background.hitBo)) {
-                isTouching = true;
-                colliding = x;
-                y+=1; //CHECK AT HOME HELP
                 return true;
             }
         }
-        isTouching = false;
         //System.out.println("FALSE");
         return false;
     }
-    public static void move() {
 
-        if (wall()) {
-            x -= dx*2;
-        } else {
-            x += dx*5;
+    public static void move() {
+        if(Facing){
+            x+=2;
+            if(wall()){
+                x-=2;
+                y+=1;
+                Facing = false;
+            }
         }
-        //y += gravity;}
-//        if (isIntersecting(background.hitBoxes)) {
-//            System.out.println("HAP");
-//            wall();
-//            x += -dx * speed;
-//            y += -dy * speed*1.5;
-//                    //y += gravity;}
-//            }
+        if(!Facing){
+            x-=2;
+            if(wall()){
+                x-=2;
+                y+=1;
+                Facing = true;
+            }
+        }
     }
     public static boolean wall() {
         if (x + dx < 0) {
