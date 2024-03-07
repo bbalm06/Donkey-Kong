615deb0fc494c42aaf3af5adffa6e0231932055b
Brandon Balmos <98839318+bbalm06@users.noreply.github.com>
2024-03-07 13:37:47 -0600

    Add files via upload

---
+++ Graph_BFS.java
@@ -0,0 +1,84 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Graph_BFS
+{
+   Map<String, Set<String>> m;
+   //define a hashmap
+   
+   public void run() throws Exception
+   {
+      System.out.println( "Graph_BST" );
+
+
+
+      Scanner in = new Scanner( new File("graph.dat") );
+      m = new HashMap<>();
+
+      int c = in.nextInt();
+      in.nextLine();
+      System.out.println( c );
+
+      for( int r = 0; r < c; r++ )
+      {
+         String s = in.nextLine();
+         String[] sr = s.split(" ");
+         if(m.get(sr[0]) == null){
+            Set<String> con = new HashSet<>();
+            con.add(sr[1]);
+            m.put(sr[0],con);
+         }
+         else{
+            m.get(sr[0]).add(sr[1]);
+         }
+         if(m.get(sr[1]) == null){
+            Set<String> con = new HashSet<>();
+            con.add(sr[0]);
+            m.put(sr[1],con);
+         }
+         else{
+            m.get(sr[1]).add(sr[0]);
+         }
+      }
+      System.out.println(m);
+
+      c = in.nextInt();
+      in.nextLine();
+      for( int r = 0; r < c; r++ )
+      {
+         String s = in.nextLine();
+         String[] st = s.split(" ");
+         ArrayList<String> visited = new ArrayList<>();
+         //read in pair to check
+         Queue<String> Q = new LinkedList<>();
+         //make a visited list - will store each node after you visit it
+         Q.add(st[0]);
+         visited.add(st[0]);
+         //make a new queue
+         //add starting node to the queue and visited list
+          while(!Q.isEmpty()){
+
+          }
+         //while the queue is not empty
+         
+            //get the first node from the queue
+            
+            //get first nodes connections
+            
+            //loop through the connection
+            
+               //if you have not already visited this node
+               //see if it is a match 
+               //add stuff to the queue and to visited
+ 
+      }
+   }
+   
+   public static void main( String[] args ) throws Exception
+   {
+     Graph_BFS a = new Graph_BFS();
+     a.run();   
+   }
+}
+
